9a8b14d2c0e103de435bacbe79abc8fc5a51869e
RafaelJrr <162131453+RafaelJrr@users.noreply.github.com>
2024-03-04 14:21:35 +0000

    Versão 03

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void dizerBomdia (char nome[20]){
+printf("Bom dia! %s \n", nome);
+}
+  
+int main(void) {
+  dizerBomdia("Rafael");
+  dizerBomdia("Marli low fade");
+  
+  return 0;
+}
